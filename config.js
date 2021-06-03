f06fb91abc286239d99b3eab4e8e66706294c7c2
trangdayok <85279672+trangdayok@users.noreply.github.com>
2021-06-03 14:47:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ const CONFIG = {
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Ok cậu,Tiếp Tục',
     title: 'Phải chăng tôi đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    desc: 'Phải chăng tôi đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vâng tôi sẽ yêu thương cậu,❤️ ',
     btnNo: 'Không, cậu à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao câụ lại yêu tôi <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Tôi biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
-    messDesc: ' Vậy câuj phải ở bên cạnh tôi nhá ❤️ ',
+    messDesc: ' Vậy cậu phải ở bên cạnh tôi nhá ❤️ ',
     btnAccept: 'Okiiiii lun <3',
 
 }
